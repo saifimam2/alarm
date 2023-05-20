75244c4b89aa62ea309a5f25c76f51a1f73a916f
saifimam2 <133911725+saifimam2@users.noreply.github.com>
2023-05-20 21:56:08 +0530

    script.js

--- script.js
+++ script.js
@@ -227,7 +227,7 @@ list=document.querySelectorAll(".alarm");
       if(med=="pm"){
          if(hour!=12)
          {
-         thour=17;
+         thour=hour+12;
       }
       else{
          thour=hour;
